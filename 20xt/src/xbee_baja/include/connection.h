ce644bd0d1b8fb4ecd09199ee260a195e9bf49b2
Tanner Gladson <tannergladson@gmail.com>
2024-01-07 16:40:39 -0500

    Defined the connection abstraction

---
+++ 20xt/src/xbee_baja/include/connection.h
@@ -0,0 +1,31 @@
+#ifndef REMOTE_CONNECTION_H
+#define REMOTE_CONNECTION_H
+
+#include "../proto/baja_live_comm.pb.h"
+
+class Connection {
+public:
+  virtual int open() = 0;
+  virtual bool is_open() const = 0;
+  virtual void close() = 0;
+
+  enum SendResult {
+    SUCCESS,             // send was successful
+    MSG_TOO_LARGE,       // Message is too large
+    QUEUE_FULL,          // send queue is full
+    SEND_FAILED,         // unkown, recoverable error occured
+    CONNECTION_BROKEN,   // irrecoverable error occured
+  };
+  virtual SendResult tx_status() const = 0;
+  virtual SendResult send(const LiveComm) = 0;
+
+  enum RecieveStatus {
+    MSG_AVAILABLE,      // messages are available for consumption
+    NO_MESSAGES,        // no messages available
+    CONNECTION_BROKEN,  // an irrecoverable error occured in connection
+  };
+  virtual RecieveStatus rx_status() = 0;
+  virtual LiveComm get_message() = 0;
+};
+
+#endif // REMOTE_CONNECTION_H
