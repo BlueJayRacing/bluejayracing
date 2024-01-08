53edf1052e54c6406f9bb32541abc3e661a8d25b
Tanner Gladson <tannergladson@gmail.com>
2024-01-08 14:35:47 -0500

    Scaffolded inter-thread communication

---
+++ 20xt/src/xbee_baja/include/queue_manage.h
@@ -0,0 +1,26 @@
+#ifndef QUEUE_FUNCS
+#define QUEUE_FUNCS
+
+#include "trx_queues.h"
+
+// Adapter functions allowing the xbee-driver loop to easily
+// manage the TRX_QUEUES with native data. Parse into LiveComm objects
+// or parsing data from LiveComm objects and putting that data 
+// into the queues.
+
+// Minimum number of payloads available overall
+int num_payloads_available(TRXProtoQueues* queues);
+
+// Min number of payloads available for a given field
+int num_payloads_available(int field_id);
+
+// Build up to a max size LiveComm object out of data from one field
+LiveComm build_message(int field_id);
+
+// Build up to a max size LiveComm object out of data from all fields
+LiveComm build_message(TRXProtoQueues* queues);
+
+// Build up to a max size LiveCOmm objject out of data from a vector of fields
+LiveComm build_message(std::vector<int> field_ids);
+
+#endif // QUEUE_FUNCS
