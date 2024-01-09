eed7136e111bbd8896c82a351f1b794e068b63f1
Tanner <tannergladson@gmail.com>
2024-01-09 18:08:33 +0000

    Updated to modern mutexes

--- 20xt/src/xbee_baja/src/safe_queue.cpp
+++ 20xt/src/xbee_baja/src/safe_queue.cpp
@@ -1,22 +1,16 @@
-#include <cassert>
-#include <ctime>
-#include <pthread.h>
-#include <safe_queue.h>
+#include <thread>
+#include <mutex>
+#include "safe_queue.h"
 #include "guard.h"
 
 template <typename T>
-SafeQueue<T>::SafeQueue(int max_size) {
-  pthread_mutex_init(&dequeue_lock, NULL);
-  pthread_mutex_init(&enqueue_lock, NULL);
+SafeQueue<T>::SafeQueue(int max_size) : {
   this->max_size = max_size;
 }
 
 // Destroy sempahore, lock, and messages
 template <typename T>
 SafeQueue<T>::~SafeQueue() {
-  pthread_mutex_destroy(&dequeue_lock);
-  pthread_mutex_destroy(&enqueue_lock);
-
   // Clean up dynamically allocated elements in the queue
   for (const auto& item : data) {
     delete item;
@@ -31,8 +25,8 @@ int SafeQueue<T>::get_size() const {
 // Enqueue a message pointer. Critical section.
 template <typename T>
 void SafeQueue<T>::enqueue(T data) {
-  Guard guard(enqueue_lock);
-
+  std::scoped_lock guard(enqueue_lock);
+  
   T* data_ptr = new T(data);
   if (size.load() < max_size) {
     data.push_back(data_ptr);
@@ -43,10 +37,11 @@ void SafeQueue<T>::enqueue(T data) {
 // Dequeue a message pointer. Critical section.
 template <typename T>
 T SafeQueue<T>::dequeue() {
-  Guard guard(dequeue_lock);
+  std::scoped_lock guard(dequeue_lock);
+
   if (data.empty()) {
     return NULL;
-  }
+  } 
   
   T d = *data.front();
   data.pop_front();
