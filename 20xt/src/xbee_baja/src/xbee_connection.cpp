d263616b4a7c10ada86ed776da036feaf42c4eb2
Tanner <tannergladson@gmail.com>
2024-01-09 21:23:06 +0000

    Comment guides for xbee_connection

--- 20xt/src/xbee_baja/src/xbee_connection.cpp
+++ 20xt/src/xbee_baja/src/xbee_connection.cpp
@@ -1,17 +1,34 @@
-
-
 #include "xbee_connection.h"
+extern "C" {
+  #include "platform_config.h"
+  #include "xbee/device.h"
+  #include "xbee/atcmd.h"
+  #include "xbee/wpan.h"
+}
+
+// TODO: write the frame handlers
+
+static const uint32_t  BAUD_RATE = 921600;
+static const std::string  SERIAL_DEVICE_ID = "/dev/ttyS0";
+static const int  MAX_PAYLOAD_SIZE = 100;
 
 XBeeConnection::XBeeConnection(){
-  
+  // TODO: dynamically allocate the RX queue
 }
 
 XBeeConnection::~XBeeConnection(){
+  // TODO: delete the RX queue
 }
 
 
 Connection::Status XBeeConnection::open(){
   return Connection::IRRECOVERABLE_ERROR;
+
+  // TODO: Initialize this->xbee using the init_baja_xbee() function
+  //       ... (see testConfigureXbee.h/c for example)
+
+  this->conn_open = true
+  return CONNECTION::SUCCESS;
 }
 
 bool XBeeConnection::is_open() const {
@@ -19,27 +36,40 @@ bool XBeeConnection::is_open() const {
 }
 
 void XBeeConnection::close(){
+  // TODO: figure out how to destroy the xbee
   
+  this->conn_open = false;
+  return;
 }
 
 Connection::Status XBeeConnection::tx_status() const{
   return Connection::IRRECOVERABLE_ERROR;
+  // TODO: Check if the XBee's serial buffer is full (return if true)
+  // TODO: Check if last message was sent successfully (return if true)
 }
 
 Connection::Status XBeeConnection::send(const std::string msg){
   return Connection::IRRECOVERABLE_ERROR;
+  // TODO: convert message to a c-string
+  // TODO: Assert that the message is less than MAX_PAYLOAD_SIZE
+  // TODO: Check if the XBee's serial buffer is full
+
+  // TODO: Construct the xbee_header_transmit_explicit_t
+  // TODO: Send the message using xbee_frame_write()
+  // TODO: return success if message sent over serial
 }
 
 Connection::Status XBeeConnection::tick(){
   return Connection::IRRECOVERABLE_ERROR;
+  // TODO: Tick the xbee, handle possible failures
 }
 
 int XBeeConnection::num_messages_available() const{
-  return 0;
+  return rx_queue->size();
 }
 
 std::string XBeeConnection::pop_message(){
-  return NULL;
+  return rx_queue->pop();
 }
 
 
