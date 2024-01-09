a00f7bc225976b68b4c7fb6205fa6ecc72717004
Tanner <tannergladson@gmail.com>
2024-01-09 23:04:45 +0000

    Wrote XbeBajaConfig file

---
+++ 20xt/src/xbee_baja/include/xbee_baja_config.h
@@ -0,0 +1,24 @@
+#ifndef XBEE_BAJA_CONFIGS_H
+#define XBEE_BAJA_CONFIGS_H
+
+  namespace XbeeBajaConfig {
+    struct cmd {
+    const char* name;
+    const int value;
+    };
+
+    static const long XBEE_BAJA_CM = 0x3FFFFFFFFFFFF; // Channel mask
+    static const int XBEE_BAJA_BD = 921600; // Channel mask
+
+    static const struct cmd XBEE_BAJA_CONFIGS[] = {
+    {"HP", 0},
+    {"TX", 2},
+    {"BR", 1},
+    {"AP", 1},
+    {"ID", 2015},
+    {"MT", 0},
+    {"NP", 100},
+  };
+}
+
+#endif
