04ac431e88b1ca7b7d19e6cf965b4ee986e970d6
Tanner <tannergladson@gmail.com>
2024-01-11 23:12:27 +0000

    started dummy implementation

--- 20xt/src/xbee_baja/src/queue_manage.cpp
+++ 20xt/src/xbee_baja/src/queue_manage.cpp
@@ -2,26 +2,19 @@
 
 // Minimum number of payloads available overall
 int num_payloads_available(TRXProtoQueues* queues) {
-  return 0; // TODO
-}
-
-// Min number of payloads available for a given field
-int num_payloads_available(int field_id) {
-  return 0; // TODO
-}
-
-// Build up to a max size LiveComm object out of data from one field
-LiveComm build_message(int field_id) {
-  return LiveComm(); // TODO
+  return 1; // TODO
 }
 
 // Build up to a max size LiveComm object out of data from all fields
 LiveComm build_message(TRXProtoQueues* queues) {
-  return LiveComm(); // TODO
-}
-
-// Build up to a max size LiveCOmm objject out of data from a vector of fields
-LiveComm build_message(std::vector<int> field_ids) {
+  // TODO: This is a dummy implementation
+  // GPS gps;
+  // gps.set_alt(1.5);
+  // gps.set_lat(2.5);
+  // gps.set_long_(3.5);
+  // LiveComm msg;
+  // msg.set_allocated_gps(&gps);
+  
   return LiveComm(); // TODO
 }
 
