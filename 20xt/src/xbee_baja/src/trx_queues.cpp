4cf6cf869fd7463413616e04f71780a2145e7cb8
Tanner <tannergladson@gmail.com>
2024-01-12 02:06:00 +0000

    Attempted TRX queues implementation

--- 20xt/src/xbee_baja/src/trx_queues.cpp
+++ 20xt/src/xbee_baja/src/trx_queues.cpp
@@ -1,116 +1,68 @@
 #include "ipc/trx_queues.h"
+#include "ipc/safe_queue.h"
 #include "baja_live_comm.pb.h"
 
-TRXProtoQueues::TRXProtoQueues() {
-
+TRXProtoQueues::TRXProtoQueues(int max_queue_size) {
+  queues[LiveComm::kGpsFieldNumber] = new SafeQueue<GPS>(max_queue_size);
+  queues[LiveComm::kLocalizationFieldNumber] = new SafeQueue<Localization>(max_queue_size);
+  queues[LiveComm::kCommunicationFieldNumber] = new SafeQueue<Communication>(max_queue_size);
+  queues[LiveComm::kTimestampFieldNumber] = new SafeQueue<Timestamp>(max_queue_size);
+  queues[LiveComm::kAnalogChFieldNumber] = new SafeQueue<AnalogChannel>(max_queue_size);
+  queues[LiveComm::kCarStateFieldNumber] = new SafeQueue<CarState>(max_queue_size);
 }
 
 TRXProtoQueues::~TRXProtoQueues() {
-
+  delete static_cast<SafeQueue<GPS>*>(queues[LiveComm::kGpsFieldNumber]);
+  delete static_cast<SafeQueue<Localization>*>(queues[LiveComm::kLocalizationFieldNumber]);
+  delete static_cast<SafeQueue<Communication>*>(queues[LiveComm::kCommunicationFieldNumber]);
+  delete static_cast<SafeQueue<Timestamp>*>(queues[LiveComm::kTimestampFieldNumber]);
+  delete static_cast<SafeQueue<AnalogChannel>*>(queues[LiveComm::kAnalogChFieldNumber]);
+  delete static_cast<SafeQueue<CarState>*>(queues[LiveComm::kCarStateFieldNumber]);
 }
 
-// Query number of elements queued
-int TRXProtoQueues::size_gps() {
-  return 0;
+int TRXProtoQueues::get_total_size() {
+  int total_size = 0;
+  total_size += static_cast<const SafeQueue<GPS>*>(queues[LiveComm::kGpsFieldNumber])->get_size();
+  total_size += static_cast<const SafeQueue<Localization>*>(queues[LiveComm::kLocalizationFieldNumber])->get_size();
+  total_size += static_cast<const SafeQueue<Communication>*>(queues[LiveComm::kCommunicationFieldNumber])->get_size();
+  total_size += static_cast<const SafeQueue<Timestamp>*>(queues[LiveComm::kTimestampFieldNumber])->get_size();
+  total_size += static_cast<const SafeQueue<AnalogChannel>*>(queues[LiveComm::kAnalogChFieldNumber])->get_size();
+  total_size += static_cast<const SafeQueue<CarState>*>(queues[LiveComm::kCarStateFieldNumber])->get_size();
+  return total_size;
 }
 
-int TRXProtoQueues::size_localization() {
-  return 0;
+template <typename T>
+int TRXProtoQueues::get_size(int field_number) const {
+  if (queues.at(field_number) == nullptr) {
+    throw std::runtime_error("Queue not initialized");
+  }
+  return static_cast<SafeQueue<T>*>(queues.at(field_number))->get_size();
 }
-
-int TRXProtoQueues::size_communication() {
-  return 0;
-}
-
-int TRXProtoQueues::size_timestamp() {
-  return 0;
-}
-
-int TRXProtoQueues::size_analog_channel() {
-  return 0;
-}
-
-int TRXProtoQueues::size_car_state() {
-  return 0;
-}
-
-
 
 // Enqueue data in its native format
-void TRXProtoQueues::enqueue(GPS data) {
-
+template <typename T>
+void TRXProtoQueues::enqueue(int field_number, T data) {
+  if (queues.at(field_number) == nullptr) {
+    throw std::runtime_error("Queue not initialized");
+  }
+  static_cast<SafeQueue<T>*>(queues.at(field_number))->enqueue(data);
 }
-
-void TRXProtoQueues::enqueue(Localization data) {
-
-}
-
-void TRXProtoQueues::enqueue(Communication data) {
-
-}
-
-void TRXProtoQueues::enqueue(Timestamp data) {
-
-}
-
-void TRXProtoQueues::enqueue(AnalogChannel data) {
-
-}
-
-void TRXProtoQueues::enqueue(CarState data) {
-
-}
-
 
 // Peek the first value
-GPS TRXProtoQueues::peek_gps() {
-  return GPS();
+template <typename T>
+T TRXProtoQueues::peek(int field_number) {
+  if (queues.at(field_number) == nullptr) {
+    throw std::runtime_error("Queue not initialized");
+  }
+  return static_cast<SafeQueue<T>*>(queues.at(field_number))->peek();
 }
-
-Localization TRXProtoQueues::peek_localization() {
-  return Localization();
-}
-
-Communication TRXProtoQueues::peek_communication() {
-  return Communication();
-}
-
-Timestamp TRXProtoQueues::peek_timestamp() {
-  return Timestamp();
-}
-
-AnalogChannel TRXProtoQueues::peek_analog_channel() {
-  return AnalogChannel();
-}
-
-CarState TRXProtoQueues::peek_car_state() {
-  return CarState();
-}
-
 
 // Deque data in its native format
-GPS TRXProtoQueues::dequeue_gps() {
-  return GPS();
+template <typename T>
+T TRXProtoQueues::dequeue(int field_number) {
+  if (queues.at(field_number) == nullptr) {
+    throw std::runtime_error("Queue not initialized");
+  }
+  return static_cast<SafeQueue<T>*>(queues.at(field_number))->dequeue();
 }
-
-Localization TRXProtoQueues::dequeue_localization() {
-  return Localization();
-}
-
-Communication TRXProtoQueues::dequeue_communication() {
-  return Communication();
-}
-
-Timestamp TRXProtoQueues::dequeue_timestamp() {
-  return Timestamp();
-}
-
-AnalogChannel TRXProtoQueues::dequeue_analog_channel() {
-  return AnalogChannel();
-}
-
-CarState TRXProtoQueues::dequeue_car_state() {
-  return CarState();
-}
-
 
