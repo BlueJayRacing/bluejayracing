7bb8fb684160213a591dc7dd1b4a354a7174f40e
Tanner <tannergladson@gmail.com>
2024-01-13 17:41:18 +0000

    renamed for clarity

--- 20xt/src/xbee_baja/include/ipc/safe_queue.h
+++ 20xt/src/xbee_baja/include/ipc/safe_queue.h
@@ -9,59 +9,72 @@
 #include "interfaces/live_comm_queue.h"
 
 
-// If cannot get to work, consider using oneTBB concurrent_bounded_queue<T, alloc> instead
-// A thread-safe queue that can be accessed concurrently by enqueing
-// and dequeing threads.
 
-class SafeQueue : public LiveCommQueue {
+// This is a thread-safe queue that can be used to pass data between threads.
+// Be wary when using front() and then dequeue() as another consumer might have
+// stolen your data
+class SafeLiveCommQueue : public LiveCommQueue {
 private:
   const int max_size;
-  std::atomic<int> curr_size;
+  std::atomic<int> tail; // Oldest element
+  std::atomic<int> head; // Newest element
+  std::atomic<int> qlen;
+
   std::mutex dequeue_lock; // must be held while dequeueing
   std::mutex enqueue_lock; // must be held while enqueueing
-  std::deque<LiveComm> data_queue;
+  LiveComm* data_queue;
 
 public:
 
-  SafeQueue(int max_size) : max_size (max_size) {}
-  ~SafeQueue() {}
-
-  int size() {
-    return curr_size.load();
+  SafeLiveCommQueue(int max_size) : max_size (max_size), qlen(0), head(-1) {
+    data_queue = new LiveComm[max_size];
   }
 
+  ~SafeLiveCommQueue() {
+    delete[] data_queue;
+  }
+
+  int size() {
+    return qlen.load();
+  }
+
+  // Critical Section
   bool enqueue(LiveComm data) {
     std::scoped_lock guard(enqueue_lock);
-    if (curr_size.load() >= max_size) {
+    if (qlen.load() >= max_size) {
       return false;
     };
 
-    curr_size += 1;
-    this->data_queue.push_back(data);
+    qlen += 1; // Claim more length below filling it
+    head = (head.load() + 1) % max_size;
+    data_queue[head.load()] = data;
     return true;
   }
 
+  // Critical Section
   LiveComm front() {
     std::scoped_lock guard(dequeue_lock);
-    if (curr_size.load() <= 0) {
+    if (qlen.load() <= 0) {
       std::cerr << "ERROR: queue is empty, returning default value" << std::endl;
       return LiveComm();
     } 
-    return this->data_queue.front();
+
+    return data_queue[tail.load()];
   }
 
 
+  // Critical Section
   LiveComm dequeue() {
     std::scoped_lock guard(dequeue_lock);
-    if (curr_size.load() <= 0) {
+    if (qlen.load() <= 0) {
       std::cerr << "ERROR: queue is empty, returning default value" << std::endl;
       return LiveComm();
     } 
     
-    LiveComm d = this->data_queue.front();
-    this->data_queue.pop_front();
-    curr_size -= 1;
-    return d;
+    LiveComm data = data_queue[tail.load()];
+    tail = (tail.load() + 1) % max_size;
+    qlen -= 1; // Must reduce length after retrieving data
+    return data;
   }
 };
 
