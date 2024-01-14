2eac110c99629905b4cc822247dcba5969651360
Tanner Gladson <tannergladson@gmail.com>
2024-01-13 19:03:40 -0500

    Built the LiveComm factory

---
+++ 20xt/src/xbee_baja/include/ipc/live_comm_factory.h
@@ -0,0 +1,25 @@
+#ifndef LIVE_COMM_FACTORY_H
+#define LIVE_COMM_FACTORY_H
+
+#include "baja_live_comm.pb.h"
+
+// Make a LiveCommFactory which will synthesize a LiveComm object as a series of Observation objects is provided. 
+// If an Observation object with GPS is provided, and the previous Oberservation already has GPS added, then 
+// we'll append another observation to the internal LiveComm
+class LiveCommFactory {
+public:
+  LiveCommFactory();
+  ~LiveCommFactory() {}
+
+  // Add a single field from an observation to the LiveComm
+  void add_observation(int field_id, Observation observation);
+
+  // Get the LiveComm object
+  LiveComm get_live_comm();
+
+private:
+  LiveComm live_comm;
+  Observation *current_observation;
+};
+
+#endif
