91f1f50ed3cb90fe14d9c95baad46b4406ed03ce
Tanner Gladson <tannergladson@gmail.com>
2024-01-20 16:58:55 -0500

    Swapped to using a single channel RX shared queue

--- 20xt/src/xbee_baja/src/trx_dispatcher.cpp
+++ 20xt/src/xbee_baja/src/trx_dispatcher.cpp
@@ -10,9 +10,23 @@
 int dispatcher_main_loop(TRXProtoQueues& shared_tx_queue, TRXProtoQueues& shared_rx_queue,
                          const mqd_t ipc_tx_queue, const std::vector<mqd_t> &ipc_rx_queues)
 {
+  char buffer[StationIPC::MAX_MSG_SIZE];
+  // Attempt to read a message from ipc_tx_queue. 
+
+  ssize_t err = mq_receive(ipc_tx_queue, buffer, StationIPC::MAX_MSG_SIZE, NULL);
+  if (err == -1 && errno != EAGAIN) {
+    std::cerr << "Could not read message from TX IPC queue. Errno " << errno << std::endl;
+  } else {
+
+  }
   
-  // Attempt to read a message from ipc_tx_queue. Deserialize it and push it to shared_tx_queue
+  // Deserialize it 
+  // For each field that is has set
+  // Push it into that queue
+
 
   // Attempt to read a message from shared_rx_queue. Serialize it and push it to all of the ipc_rx_queues
   return 0;
 }
+
+
