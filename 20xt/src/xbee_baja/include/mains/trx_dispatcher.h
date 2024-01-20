40e487ccf69ac0950cea76117be319aa3a272250
Tanner Gladson <tannergladson@gmail.com>
2024-01-20 17:00:56 -0500

    Missed a rename

--- 20xt/src/xbee_baja/include/mains/trx_dispatcher.h
+++ 20xt/src/xbee_baja/include/mains/trx_dispatcher.h
@@ -3,7 +3,7 @@
 
 #include "crossthread/trx_queues.h"
 
-int dispatcher_main_loop(TRXProtoQueues& shared_tx_queue, TRXProtoQueues& shared_rx_queue, const int ipc_tx_queue, const std::vector<int> &ipc_rx_queues);
+int dispatcher_main_loop(TRXProtoQueues& shared_tx_queue, LiveCommQueue& shared_rx_queue, const int ipc_tx_queue, const std::vector<int> &ipc_rx_queues);
 
 void _try_queue_data_for_transmit(TRXProtoQueues& shared_tx_queue, const mqd_t ipc_tx_queue);
 void _try_dispatch_recieved_data(LiveCommQueue& shared_rx_queue, const std::vector<mqd_t> &ipc_rx_queues);
