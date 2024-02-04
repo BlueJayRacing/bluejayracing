d11a31fcc1f817d9135ee5b434ec3319235527ed
Tanner Gladson <tannergladson@gmail.com>
2024-02-04 00:33:57 +0000

    renamed again

---
+++ 20xt/scripts/station_docker_run_prod.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+echo $SCRIPTPATH
+
+if ! docker images | grep -q station_docker; then
+    sh $SCRIPTPATH/station_docker_build.sh
+fi
+
+if ! docker ps | grep -q station_docker; then
+    docker run \
+        -id \
+        --mount type=bind,source=$SCRIPTPATH/../.station_build_cache,target=/20xt_ws/bin \
+        --mount type=bind,source=$SCRIPTPATH/../src,target=/20xt_ws/src \
+        -it --device=/dev/ttyS0 \
+        station_docker > /dev/null
+fi
+
+docker container exec -it $(docker ps | grep station_docker | awk '{print $NF}') /bin/bash
