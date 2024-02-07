ff5a21588f05a6a0f9299c6beea27ba1e8c1da03
Tanner Gladson <tannergladson@gmail.com>
2024-02-07 01:56:20 +0000

    Wrote mocks for testing

---
+++ 20xt/src/base/tests/test_stress_rx_dispatcher.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <mqueue.h>
+#include <unistd.h>
+#include <chrono>
+#include <vector>
+
+#include "mains/receive_dispatcher.h"
+#include "helpers/ipc_config.h"
+#include "proto_helpers.h"
+
+// This is the recieve logic for stress testing the XBee. It mocks
+// the rx dispatching program
+
+int main()
+{
+  std::cout << "starting receive dispatcher" << std::endl;
+  // Open the queues
+  const mqd_t radio_rx_queue = StationIPC::open_queue(StationIPC::XBEE_DRIVER_RX_QUEUE);
+  if (radio_rx_queue == -1) {
+    std::cout << "Failed to get radio queue. Errno " << errno << std::endl;
+    return EXIT_FAILURE;
+  }
+  
+  // Main loop
+  int bytes_read_this_window;
+  auto start = std::chrono::high_resolution_clock::now();
+
+  while (true) {
+    std::string msg = StationIPC::get_message(radio_rx_queue);
+    bytes_read_this_window += msg.size();
+
+    time_elapsed = std::chrono::high_resolution_clock::now() - start;
+    if (time_elapsed.count() >= 30.0) {
+      std::cout << bytes_read_this_window << "bytes read in last 30 seconds"  << std::endl;
+      bytes_read_this_window = 0;
+      auto start = std::chrono::high_resolution_clock::now();
+    }
+  }
+  return 0;
+}
---
+++ 20xt/src/base/tests/test_stress_tx_prioritizer.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <mqueue.h>
+#include <unistd.h>
+#include <vector>
+
+#include "mains/transmit_prioritizer.h"
+#include "baja_live_comm.pb.h"
+#include "helpers/ipc_config.h"
+#include "helpers/live_comm_factory.h"
+
+// Use this process to mock the transmit prioritizer program. Always
+// sends 100 byte messages to the radio queue. Intended to stress test
+// the XBee
+
+static const std::string TEST_MESSAGE = "This is a 100 byte string yk. It is like writing an essay and trying to get the exact 100 word count"
+static Observation* remainder_data = nullptr;
+
+int main()
+{
+  std::cout << "starting stress test prioritzer" << std::endl;
+  
+  // Open queues
+  const mqd_t radio_queue = StationIPC::open_queue(StationIPC::XBEE_DRIVER_TO_TX_QUEUE);
+  if (radio_queue == -1) {
+    std::cout << "Failed to get radio queue. Errno " << errno << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  // We're using POSIX queues, so sending a message is NOT STATELESS
+  while (true) {
+    int result = StationIPC::send_message(radio_queue, TEST_MESSAGE);
+    if (result == StationIPC::QUEUE_FULL) {
+      StationIPC::get_message(radio_queue);
+      result = StationIPC::send_message(radio_queue, TEST_MESSAGE);
+    }
+    if (result == StationIPC::SEND_ERROR) {
+      std::cerr << "Could not enqeue message" << std::endl;
+    }
+  }
+
+  return EXIT_SUCCESS;
+}
