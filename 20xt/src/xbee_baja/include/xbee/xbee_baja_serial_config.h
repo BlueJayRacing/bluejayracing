4477fe4b095919e4eb16393992afc81855bcd2da
Tanner Gladson <tannergladson@gmail.com>
2024-02-17 12:35:02 -0500

    typo in device id

--- 20xt/src/xbee_baja/include/xbee/xbee_baja_serial_config.h
+++ 20xt/src/xbee_baja/include/xbee/xbee_baja_serial_config.h
@@ -5,7 +5,7 @@
 
 namespace XbeeBajaSerialConfig {
   static const int BAUDRATE = 921600;
-  static const std::string STATION_DEVICE = "/dev/ttyUSB0";
+  static const std::string STATION_DEVICE = "/dev/ttyS0";
   static const std::string CAR_DEVICE = "PUT CAR DEVICE HERE";
 };
 
