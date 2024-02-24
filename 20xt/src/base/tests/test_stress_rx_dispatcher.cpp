f893cb504001b165b1439f17edd9ac67ce5a15f1
Tanner Gladson <tannergladson@gmail.com>
2024-02-24 16:44:27 +0000

    report by message num instead

--- 20xt/src/base/tests/test_stress_rx_dispatcher.cpp
+++ 20xt/src/base/tests/test_stress_rx_dispatcher.cpp
@@ -28,18 +28,16 @@ int main()
   auto start = std::chrono::high_resolution_clock::now();
 
   int WINDOW_SIZE = 5;
+  int msg_num = 0;
   while (true) {
     std::string msg = StationIPC::get_message(radio_rx_queue); // blocking
+    msg_num++;
     bytes_read_this_window += msg.size();
 
     auto time_elapsed = std::chrono::high_resolution_clock::now() - start;
     auto time_elapsed_seconds = std::chrono::duration_cast<std::chrono::seconds>(time_elapsed).count();
     if (time_elapsed_seconds >= WINDOW_SIZE) {
-      // Get the current time
-      std::time_t now = std::time(0);
-      char* dt = std::ctime(&now);
-
-      std::cout << "Received at rate of " << bytes_read_this_window / WINDOW_SIZE << " bytes/second at time " << dt;
+      std::cout << "Received at rate of " << bytes_read_this_window / WINDOW_SIZE << " bytes/second up to message #" << msg_num << std::endl;
       bytes_read_this_window = 0;
       start = std::chrono::high_resolution_clock::now();
     }
