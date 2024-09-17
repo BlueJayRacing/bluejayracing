bdfa8b0b6261d68de3893cffdb510a45de3a7182
Travis Yu Chen <147548360+TravdaTrav@users.noreply.github.com>
2024-09-17 19:53:53 -0400

    merging 2024 michigan firmware to rmain repo (#22)

---
+++ 20xt/firmware/esp_idf/components_cpp/ad5626/include/ad5626.hpp
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef _AD5626_HPP_
+#define _AD5626_HPP_
+
+#include <esp_system.h>
+#include <driver/gpio.h>
+#include <driver/spi_master.h>
+
+class AD5626 {
+    public:
+        AD5626();
+        esp_err_t init(const gpio_num_t t_cs_pin, const gpio_num_t t_ldac_pin, const gpio_num_t t_clr_pin, const spi_host_device_t t_spi_host);
+        esp_err_t setLevel(const uint16_t t_new_dac_level);
+        esp_err_t clearLevel(void);
+    private:
+        spi_device_handle_t m_spi_dev;
+        gpio_num_t m_ldac_pin;
+        gpio_num_t m_clr_pin;
+};
+
+#endif
