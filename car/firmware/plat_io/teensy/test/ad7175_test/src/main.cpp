9db190d049165752421f6d718f80ad7a338f3837
jemmalemon <ouyangemma44@gmail.com>
2025-01-18 14:05:01 -0500

    Commented files & edited for unipolar

--- car/firmware/plat_io/teensy/test/ad7175_test/src/main.cpp
+++ car/firmware/plat_io/teensy/test/ad7175_test/src/main.cpp
@@ -19,19 +19,23 @@ void setup()
     delay(2000);
     Serial.begin(115200);
     SPI.begin();
+    Serial.println("Hello");
 
     pinMode(1, OUTPUT);
 
     ad717x_init_param_t ad7175_params;
 
-    ad717x_analog_inputs_t chan_0_inputs;
-    chan_0_inputs.ainp.pos_input = AIN10;
-    chan_0_inputs.ainp.neg_input = AIN9;
+    ad717x_analog_inputs_t chan_0_inputs; 
+    chan_0_inputs.ainp.pos_input = AIN15; // Change while testing
+    chan_0_inputs.ainp.neg_input = AIN6;
 
-    ad717x_channel_map_t chan_0_map       = {true, 0, chan_0_inputs};
-    ad717x_channel_config_t chan_0_config = {true, false, false, AVDD_AVSS};
-    ad717x_filter_config_t filt_con       = {false, false, SPS27_DB47_MS36P7, SINC5_SINC1, SPS_100};
-    ad717x_setup_t setup                  = {0.5, chan_0_config, filt_con};
+    // Change while testing
+    ad717x_channel_map_t chan_0_map       = {true, 0, chan_0_inputs}; // enable, setup_index, input selection
+    ad717x_channel_config_t chan_0_config = {false, false, true, EXTERNAL_REF}; // bipolar, ref_buff, input_buff, ref_source
+    // sinc3 map, bool enhfilten, enhfilten, oder, odr
+    ad717x_filter_config_t filt_con       = {false, false, SPS27_DB47_MS36P7, SINC5_SINC1, SPS_100}; 
+    // gain, setup, filter_config
+    ad717x_setup_t setup                  = {1, chan_0_config, filt_con};
 
     ad7175_params.active_device = ID_AD7175_8;
     ad7175_params.ref_en        = true;
@@ -50,7 +54,8 @@ void loop()
     ad7175.waitForReady(0xFFFFFFFF);
     ad7175.readData(&data_val);
 
-    voltage_val = ((double)(data_val)-CENTER) / CENTER * 5;
+    voltage_val = ((double)(data_val) * 5) / (std::pow(2, 24));
+    // voltage_val = ((double)(data_val)-CENTER) / CENTER * 5;
     num++;
 
     Serial.println(voltage_val, 6);
