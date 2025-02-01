e05fbc476e7437823130add76e2b6d690e3d4b8e
Cyan Brown <71455691+CyanBrown@users.noreply.github.com>
2025-02-01 14:27:29 -0600

    AD5689R firmware library (#39)
    ## Pull Request
    * **Please check**
    - [x] We have discussed the changes
    - [x] Tests for the changes have been done
    - [x] Docs have been added / updated
    
    **OR**
    - [ ] Quick Patch adding Tech Debt
    
    
    * **What kind of change does this PR introduce?** (Bug fix, feature,
    docs update, ...)
    Added AD5689R firmware library.
    
    * **What is the new behavior (if this is a feature change)?**
    
    
    * **Does this PR introduce a breaking change?** (What changes might
    users need to make in their application due to this PR?)
    No
    
    * **Other information**:
    
    ---------
    
    Co-authored-by: jemmalemon <ouyangemma44@gmail.com>
    Co-authored-by: TravdaTrav <tchen128@jh.edu>
    Co-authored-by: TravdaTrav <travis.yu.chen@gmail.com>

---
+++ car/firmware/plat_io/teensy/test/ad5689r_test/src/main.cpp
@@ -0,0 +1,40 @@
+#include <Arduino.h>
+
+#include "component_template.hpp"
+
+#include "ad5689r.hpp"
+
+AD5689R ad5689;
+uint16_t level = 1;
+double voltage_val;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  // comp_temp_func();
+
+  SPI.begin();
+
+  ad5689r_init_param_t init_params;
+  init_params.cs_pin = 10;
+  init_params.ldac_pin = 32;
+  init_params.clr_pin = 15;
+  init_params.spi_host = &SPI;
+
+  ad5689.init(init_params);
+}
+
+void loop() {
+  if (level == 0x8000) {
+    level = 1;
+  } else {
+    level = level << 1;
+  }
+
+  voltage_val = 2.5 * level * pow(2, -15);
+
+  ad5689.setLevel(A, level);
+  Serial.println(level);
+  Serial.println(voltage_val);
+  delay(4000);
+}
