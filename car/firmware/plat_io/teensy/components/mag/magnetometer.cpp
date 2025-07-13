780f56e90693887163987d26d43ac26ad0befbf3
Jeewoo Yoon <jeewooyoon@Jeewoos-MacBook-Air.local>
2025-07-13 13:47:41 -0400

    added simple magnetometer.cpp with adafruit library

--- car/firmware/plat_io/teensy/components/mag/magnetometer.cpp
+++ car/firmware/plat_io/teensy/components/mag/magnetometer.cpp
@@ -1,1 +1,43 @@
 #include "magnetometer.hpp"
+#include <Adafruit_TLV493D.h>
+
+Adafruit_TLV493D mag = Adafruit_TLV493D();
+
+void Magnetometer::begin() {
+    mag.begin();
+}
+
+bool Magnetometer::readMagData(int16_t &x, int16_t &y, int16_t &z) {
+    /*
+    // Request 7 bytes from the sensor and return, if it didn't send enough (do not read, if there is nothing to read)
+    if(Wire.requestFrom(tlv_addr, 7) < 7) return false;
+    // Read all registers to variables
+    byte bx_high = Wire.read();
+    byte by_high = Wire.read();
+    byte bz_high = Wire.read();
+    byte temp_high = Wire.read();
+    byte bxy_low = Wire.read();
+    byte bz_low = Wire.read();
+    byte temp_low = Wire.read();
+
+    unsigned int bx_value = (bx_high << 4) | ((bxy_low & 0xF0)>>4);
+    unsigned int by_value = (by_high << 4) | (bxy_low & 0x0F);
+    unsigned int bz_value = (bz_high << 4) | (bz_low & 0x0F);
+    unsigned int temp_value = (temp_high >> 4) | temp_low;
+
+    x = (int16_t)(bx_value << 4) / 16 ;
+    y = (int16_t)(by_value << 4) / 16 ;
+    z = (int16_t)(bz_value << 4) / 16 ;
+
+    return true;
+    */
+
+    mag.read();  // updates internal sensor data
+
+    //data output in Teslas
+    x = mag.xData();
+    y = mag.yData();
+    z = mag.zData();
+
+    return true;
+}
--- car/firmware/plat_io/teensy/components/mag/magnetometer.hpp
+++ car/firmware/plat_io/teensy/components/mag/magnetometer.hpp
@@ -2,21 +2,21 @@
 #define _MAGNETOMETER_HPP_
 
 #include <Arduino.h>
-#include <Wire.h>
 
 class Magnetometer {
 public:
-    Magnetometer(TwoWire &wire = Wire, uint8_t address = 0x5E);
+    //Magnetometer(TwoWire &wire = Wire, uint8_t address = 0x5E);
+    Magnetometer();
     void begin();
-
     bool readMagData(int16_t &x, int16_t &y, int16_t &z);
-
+/*
 private:
     TwoWire *_wire;
     uint8_t _address;
 
     void writeRegister(uint8_t reg, uint8_t value);
     bool readRegisters(uint8_t reg, uint8_t *buffer, uint8_t length);
+*/
 };
 
 #endif
